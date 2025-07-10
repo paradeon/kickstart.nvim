ff9c5b31cf18fef070786cb722c922f4c5df71a2
ayu <paradeon@gmail.com>
2025-07-10 10:54:56 +0800

    disable treesitter for windows

--- init.lua
+++ init.lua
@@ -958,6 +958,9 @@ require('lazy').setup({
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    enabled = function()
+      return jit.os == 'OSX'
+    end,
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
