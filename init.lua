337e1434e7747844404817daa1bcdefb95fdbed1
ayu <paradeon@gmail.com>
2025-07-10 10:49:30 +0800

    change org directory to from ~/org to ~/notes

--- init.lua
+++ init.lua
@@ -987,8 +987,8 @@ require('lazy').setup({
     config = function()
       -- Setup orgmode
       require('orgmode').setup {
-        org_agenda_files = '~/org/**/*',
-        org_default_notes_file = '~/org/refile.org',
+        org_agenda_files = '~/notes/**/*',
+        org_default_notes_file = '~/notes/refile.org',
       }
     end,
   },
